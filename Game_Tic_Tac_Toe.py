6191f197bb96725b992eb9365b8a2bc405807d40
Tansil011019 <16521295@std.stei.itb.ac.id>
2022-04-15 12:54:49 +0700

    Update 15/04/2022

--- Game_Tic_Tac_Toe.py
+++ Game_Tic_Tac_Toe.py
@@ -1,9 +1,12 @@
 def tic_tac_toe():
     chance = 9
     array = [["#" for i in range (3)] for j in range (3)]
+    valid = True
     while (chance != 0):
-        row = int(input("Masukkan baris: "))-1
-        col = int(input("Masukkan kolum: "))-1
+        row_input = int(input("Masukkan baris: "))
+        col_input = int(input("Masukkan kolum: "))
+        row =row_input - 1
+        col = col_input - 1
         if(array[row][col]=="#"):
             if(chance%2 == 0):
                 array[row][col]="O"
@@ -16,16 +19,21 @@ def tic_tac_toe():
             if(((array[0][1]==array[0][0]==array[0][2])and(array[0][1]!="#" and array[0][0]!="#" and array[0][2]!="#")) or ((array[1][1]==array[1][0]==array[1][2])and(array[1][1]!="#" and array[1][0]!="#" and array[1][2]!="#")) or ((array[2][1]==array[2][0]==array[2][2])and(array[2][1]!="#" and array[2][0]!="#" and array[2][2]!="#"))):
                 print("{} menang secara horizontal".format(array[row][col]))
                 chance = 0
+                valid = False
             elif(((array[0][0]==array[1][0]==array[2][0])and(array[0][0]!="#" and array[1][0]!="#" and array[2][0]!="#")) or ((array[0][1]==array[1][1]==array[2][1])and(array[0][1]!="#" and array[1][1]!="#" and array[2][1]!="#")) or ((array[0][2]==array[1][2]==array[2][2])and(array[0][2]!="#" and array[1][2]!="#" and array[2][2]!="#"))):
                 print("{} menang secara vertikal".format(array[row][col]))
                 chance = 0
-            elif(((array[0][0]==array[1][1]==array[2][2])and(array[0][0]!="#" and array[1][1]!="#" and array[2][2]!="#")) or ((array[0][2]==array[1][1]==array[2][1])and(array[0][2]!="#" and array[1][1]!="#" and array[2][1]!="#"))):
+                valid = False
+            elif(((array[0][0]==array[1][1]==array[2][2])and(array[0][0]!="#" and array[1][1]!="#" and array[2][2]!="#")) or ((array[0][2]==array[1][1]==array[2][0])and(array[0][2]!="#" and array[1][1]!="#" and array[2][1]!="#"))):
                 print("{} menang secara diagonal".format(array[row][col]))
                 chance = 0
+                valid = False
             else:
                 chance-=1
         else:
             print("Slot telah terisi, silahkan pilih yang lain")
+    if valid :
+        print("Game Tie")
     # for i in range(3):
     #     for j in range (3):
     #         print(array[i][j], end= " ")
