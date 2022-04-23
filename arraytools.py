86c7a2c9ca0bd2ca0cc6cf27ad2f4d894d824bfb
maggiezeta <89400437+maggiezeta@users.noreply.github.com>
2022-04-23 23:30:11 +0700

    Create arraytools.py

---
+++ arraytools.py
@@ -0,0 +1,31 @@
+def panjang_array(array):
+    length = 0
+    for i in array:
+        length += 1
+    return length
+
+
+def append(array1, array2):
+    return array1 + array2
+
+
+def printmatrix(matrix) :
+    baris = panjang_array(matrix)
+    kolom = panjang_array(matrix[0])
+    maxlencolumn = [0 for i in range(kolom+1)]
+    maxlencolumn[0] = panjang_array(str(baris-1))
+    for i in range(kolom) :
+        maks = panjang_array(matrix[1][i])
+        for j in range(2, baris) :
+            if maks < panjang_array(matrix[j][i]) :
+                maks = panjang_array(matrix[j][i])
+        maxlencolumn[i+1] = maks
+
+    for i in range(1, baris) :
+        print(str(i) + "." + (" " * (maxlencolumn[0] - panjang_array(str(i)))), end=" ")
+        for j in range(kolom) :
+            data = matrix[i][j]
+            if (j+1) != kolom :
+                print(data + (" " * (maxlencolumn[j + 1] - panjang_array(data))), end=" | ")
+            else :
+                print(data + (" " * (maxlencolumn[j + 1] - panjang_array(data))))
