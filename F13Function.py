aa272b8c7e07f058bfc00c7686957397276776d3
rifqisyafiq <89325784+rifqisyafiq@users.noreply.github.com>
2022-04-23 16:14:06 +0700

    Update F13Function.py

--- F13Function.py
+++ F13Function.py
@@ -5,5 +5,6 @@ def riwayat():
         riwayat = Parser.fill("riwayat.csv")
         print("Daftar game: ")
         for i in range (Parser.row("riwayat.csv")) :
+            if userid == riwayat [i][3] :
                 print(riwayat[i][0] + " " + riwayat[i][1] + " " + riwayat[i][2] + " "  + riwayat[i][4])
     else : print("Anda tidak memiliki hak akses. Hanya user yang boleh mengakses fitur ini.")
