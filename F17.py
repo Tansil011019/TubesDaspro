71f63d3e17b952b3c2fdbb25eee080497a958a04
Tansil011019 <16521295@std.stei.itb.ac.id>
2022-04-24 13:59:17 +0700

    Update 24/04/2022

--- F17.py
+++ F17.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import F16
 import sys
 def exit(user_file, game_file, riwayat_file, kepemilikan_file) :
@@ -12,15 +11,3 @@ def exit(user_file, game_file, riwayat_file, kepemilikan_file) :
         print()
         print("File yang sudah diubah tidak tersimpan.")
         sys.exit()
-=======
-def exit() :
-    simpan = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
-    while simpan != "y" or simpan != "n" or simpan != "Y" or simpan != "N" :
-        simpan = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
-    if simpan == "y" or simpan == "Y" :
-        save()
-    else :
-        print()
-        print("File yang sudah diubah tidak tersimpan.")
-        pass
->>>>>>> ade5631022edb7529872212d67bb0bd4d79d25d4
