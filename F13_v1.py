7e5484bdc6237dd4eeab5398c5a612c68b2a7060
rifqisyafiq <89325784+rifqisyafiq@users.noreply.github.com>
2022-04-24 09:52:01 +0700

    Update F13_v1.py

--- F13_v1.py
+++ F13_v1.py
@@ -1,15 +1,10 @@
-import Parser
 import component
-def riwayat(user_id):
-    if component.check_admin():
-        riwayat = Parser.fill("riwayat.csv")
-        count = 0
-        print("Daftar game: ")
-        for i in range (Parser.row("riwayat.csv")) :
-            if user_id == riwayat [i][3] :
-                print(riwayat[i][0] + " " + riwayat[i][1] + " " + riwayat[i][2] + " "  + riwayat[i][4])
-                count = count + 1
-        if count == 0
-            print("Tidak ada riwayat pembelian game")
-    else :
-        print("Maaf, anda tidak memiliki izin untuk menjalankan perintah berikut. Mintalah ke administrator untuk melakukan hal tersebut.")
+def riwayat(user_id,riwayat):
+    count = 0
+    print("Daftar game: ")
+    for i in range (component.row_matrix(riwayat)) :
+        if user_id == riwayat [i][3] :
+            print(riwayat[i][0] + " " + riwayat[i][1] + " " + riwayat[i][2] + " "  + riwayat[i][4])
+            count = count + 1
+    if count == 0 :
+        print("Tidak ada riwayat pembelian game")
