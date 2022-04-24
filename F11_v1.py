8ffd3197c7d04aebcaccd2d4a6f8e08b684e2d48
rifqisyafiq <89325784+rifqisyafiq@users.noreply.github.com>
2022-04-24 09:51:06 +0700

    Update F11_v1.py

--- F11_v1.py
+++ F11_v1.py
@@ -1,6 +1,6 @@
-import Parser
+import component
 
-def search_game_at_store() :
+def search_game_at_store(game) :
     # User Input parameter game yang akan dicari
     game_id = input("Masukkan ID game: ")
     name = input("Masukkan Nama game: ")
@@ -9,178 +9,177 @@ def search_game_at_store() :
     tahun_rilis = input("Masukkan Tahun Rilis Game: ")
     count = 0
     print("Daftar game pada toko yang memenuhi kriteria : ")
-    game = Parser.fill("game.csv")
     # Mengecek Validasi
     if game_id != "" and name != "" and harga == "" and kategori == "" and tahun_rilis == "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][0] == game_id and game[i][1] == name:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id != "" and name != "" and harga != "" and kategori == "" and tahun_rilis == "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][0] == game_id and game[i][1] == name and game[i][4] == harga :
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id != "" and name != "" and harga != "" and kategori != "" and tahun_rilis == "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][0] == game_id and game[i][1] == name and game[i][4] == harga  and game [i][2] == kategori :
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id != "" and name != "" and harga != "" and kategori != "" and tahun_rilis != "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][0] == game_id and game[i][1] == name and game[i][4] == harga  and game [i][2]== kategori and game [i][3] == tahun_rilis:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id != "" and name == "" and harga != "" and kategori == "" and tahun_rilis == "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][0] == game_id and game[i][4] == harga :
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id != "" and name == "" and harga != "" and kategori != "" and tahun_rilis == "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][0] == game_id and game [i][2] == kategori and game[i][4] == harga  :
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id != "" and name == "" and harga != "" and kategori != "" and tahun_rilis != "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][0] == game_id and game [i][2] == kategori and game[i][4] == harga  and game [i][3] == tahun_rilis:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id != "" and name == "" and harga == "" and kategori != "" and tahun_rilis == "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][0] == game_id and game [i]:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id != "" and name == "" and harga == "" and kategori != "" and tahun_rilis != "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][0] == game_id and game [i][2] == kategori and game [i][3] == tahun_rilis:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id != "" and name == "" and harga == "" and kategori == "" and tahun_rilis != "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][0] == game_id and game [i][3] == tahun_rilis:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name != "" and harga != "" and kategori == "" and tahun_rilis == "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][1] == name and game[i][4] == harga :
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name != "" and harga != "" and kategori != "" and tahun_rilis == "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][1] == name and game[i][4] == harga  and game [i]:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name != "" and harga != "" and kategori != "" and tahun_rilis != "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][1] == name and game[i][4] == harga  and game [i][2] == kategori and game [i][3] == tahun_rilis:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name != "" and harga == "" and kategori != "" and tahun_rilis == "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][1] == name and game [i]:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name != "" and harga == "" and kategori != "" and tahun_rilis != "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][1] == name and game [i][2] == kategori and game [i][3] == tahun_rilis:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name != "" and harga == "" and kategori == "" and tahun_rilis != "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][1] == name and game [i][3] == tahun_rilis:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name == "" and harga != "" and kategori != "" and tahun_rilis == "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][4] == harga  and game [i]:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name == "" and harga != "" and kategori != "" and tahun_rilis != "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][4] == harga  and game [i][2] == kategori and game [i][3] == tahun_rilis:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name == "" and harga != "" and kategori == "" and tahun_rilis != "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][4] == harga  and game [i][3] == tahun_rilis:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name == "" and harga == "" and kategori != "" and tahun_rilis != "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game [i][2] == kategori and game [i][3] == tahun_rilis:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name == "" and harga == "" and kategori == "" and tahun_rilis != "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game [i][3] == tahun_rilis:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id != "" and name == "" and harga == "" and kategori == "" and tahun_rilis == "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][0] == game_id:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name != "" and harga == "" and kategori == "" and tahun_rilis != "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][1] == name:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name == "" and harga != "" and kategori == "" and tahun_rilis == "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game[i][4] == harga :
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
                 if count == 0:
                     print("Game tidak tersedia pada toko")
     elif game_id == "" and name == "" and harga == "" and kategori != "" and tahun_rilis == "":
-                for i in range (Parser.row("game.csv")):
+                for i in range (component.row_matrix(game)):
                     if game [i][2] == kategori:
                         print(game[i][0] + " | " + game[i][1] + " | " + game[i][4] + " | " + game[i][2] + " | " + game[i][3] + " | " + game[i][5])
                         count = count + 1
